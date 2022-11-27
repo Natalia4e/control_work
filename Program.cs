3c24ef8b64b0482263fd3a253e2b35a1d95df25d
Natalia Cherniavskaia <natatata931@mail.ru>
2022-11-27 22:15:50 +0300

    finalwork1 done

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача. Написать программу,которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа.
+// Первоначальный массив можно ввести с клавиатуры,либо задать на старте выполнения алгоритма.
+
+string Prompt(string message)
+{
+    Console.Write(message);
+    string data = Console.ReadLine();
+    return data;
+}
+
+string message = Prompt ("Введите данные через, ");
+string[] arr = message.Split(",");
+string[] arr2 = new string[arr.Length];
+int j = 0;
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if ( arr[i].Length <= 3 )
+        {
+            arr2[j] = arr[i];
+            j++;
+        }      
+    }
+
+Console.WriteLine(String.Join(" ",arr2));
+
+
